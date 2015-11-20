5a2bed8355809a3cd89c1f3a5c8ffcd3f06c8134
dventimi <dventimi@dventimi-Lenovo-U310>
2015-11-20 04:54:54 -0800

    More work on Associations

--- atomiccore/src/main/java/org/neptunestation/atomic/core/BottomUpAtomicEdmProvider.java
+++ atomiccore/src/main/java/org/neptunestation/atomic/core/BottomUpAtomicEdmProvider.java
@@ -8,107 +8,171 @@ import org.apache.olingo.odata2.api.edm.provider.*;
 import org.apache.olingo.odata2.api.exception.*;
 
 public class BottomUpAtomicEdmProvider extends AtomicEdmProvider {
+    public static String PKTABLE_CAT = "PKTABLE_CAT";
+    public static String PKTABLE_SCHEM = "PKTABLE_SCHEM";
+    public static String PKTABLE_NAME = "PKTABLE_NAME";
+    public static String PKCOLUMN_NAME = "PKCOLUMN_NAME";
+    public static String FKTABLE_CAT = "FKTABLE_CAT";
+    public static String FKTABLE_SCHEM = "FKTABLE_SCHEM";
+    public static String FKTABLE_NAME = "FKTABLE_NAME";
+    public static String FKCOLUMN_NAME = "FKCOLUMN_NAME";
+    public static String KEY_SEQ = "KEY_SEQ";
+    public static String UPDATE_RULE = "UPDATE_RULE";
+    public static String DELETE_RULE = "DELETE_RULE";
+    public static String FK_NAME = "FK_NAME";
+    public static String PK_NAME = "PK_NAME";
+    public static String DEFERRABILITY = "DEFERRABILITY";
+
     public BottomUpAtomicEdmProvider (Properties params, String username, String password) {
         super(params, username, password);}
 
     public class AtomicRoot {
         private Map<String, AtomicSchema> schemas = new HashMap<>();
         public AtomicRoot (DatabaseMetaData m) throws NamingException, SQLException {
-	    try (ResultSet r = m.getTablePrivileges(null, null, null)) {while (r.next()) addSchema(m, r);}
-	    try (ResultSet r = m.getCrossReference(null, null, null, null, null, null)) {while (r.next()) addAssociation(m, r);}
+	    try (ResultSet r = m.getTablePrivileges(null, null, null)) {while (r.next()) addSchema(this, m, r);}
+	    try (ResultSet r = m.getCrossReference(null, null, null, null, null, null)) {while (r.next()) addAssociation(getSchema(r.getString(2)), m, r);}
 	    for (Schema s : getSchemas()) for (EntityContainer ec : s.getEntityContainers()) {ec.setDefaultEntityContainer(true); break;}}
-	public void addAssociation (DatabaseMetaData m, ResultSet r) throws SQLException {
-	    schemas.get(r.getString(2)).addEnd1Association(m, r);
-	    schemas.get(r.getString(6)).addEnd2Association(m, r);}
-        public void addSchema (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
+	public void addAssociation (AtomicSchema s, DatabaseMetaData m, ResultSet r) throws SQLException {
+	    schemas.get(r.getString(2)).addAssociation(s, m, r);}
+        public void addSchema (AtomicRoot root, DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    if (!schemas.containsKey(r.getString(2))) {
-                AtomicSchema s = new AtomicSchema(m, r);
+                AtomicSchema s = new AtomicSchema(root, m, r);
                 schemas.put(s.getNamespace(), s);}
             schemas.get(r.getString(2)).addEntityContainer(m, r);
             if (m.getUserName().equals(r.getString(5)) && "select".equalsIgnoreCase(r.getString(6))) schemas.get(r.getString(2)).addEntityType(m, r);}
+	public AtomicSchema getSchema (String name) {
+	    return schemas.get(name);}
         public List<Schema> getSchemas () {
             return new ArrayList<Schema>(schemas.values());}}
 
     public class AtomicSchema extends Schema {
+	public AtomicRoot root;
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+        private Map<String, AtomicAssociation> associations = new HashMap<>();
+        private Map<String, AtomicComplexType> complexTypes = new HashMap<>();
         private Map<String, AtomicEntityContainer> entityContainers = new HashMap<>();
         private Map<String, AtomicEntityType> entityTypes = new HashMap<>();
-        private Map<String, AtomicAssociation> associations = new HashMap<>();
-        public AtomicSchema (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
+        private Map<String, AtomicUsing> usings = new HashMap<>();
+        public AtomicSchema (AtomicRoot root, DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    super();
-	    setAnnotationAttributes(new ArrayList<AnnotationAttribute>());
-	    setAnnotationElements(new ArrayList<AnnotationElement>());
-	    setComplexTypes(new ArrayList<ComplexType>());
-	    setUsings(new ArrayList<Using>());
+	    this.root = root;
             setNamespace("" + r.getString(2));}
-	@Override public List<EntityContainer> getEntityContainers () {return new ArrayList<EntityContainer>(entityContainers.values());}
-	@Override public List<EntityType> getEntityTypes () {return new ArrayList<EntityType>(entityTypes.values());}
-	@Override public List<Association> getAssociations () {return new ArrayList<Association>(associations.values());}
-	public void addEnd1Association (DatabaseMetaData m, ResultSet r) throws SQLException {
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	@Override public List<Association> getAssociations () {
+	    return new ArrayList<Association>(associations.values());}
+	@Override public List<ComplexType> getComplexTypes () {
+	    return new ArrayList<ComplexType>(complexTypes.values());}
+	@Override public List<EntityContainer> getEntityContainers () {
+	    return new ArrayList<EntityContainer>(entityContainers.values());}
+	@Override public List<EntityType> getEntityTypes () {
+	    return new ArrayList<EntityType>(entityTypes.values());}
+	@Override public List<Using> getUsings () {
+	    return new ArrayList<Using>(usings.values());}
+	public AtomicEntityType getEntityType (String name) {
+	    return entityTypes.get(name);}
+	public AtomicEntityContainer getEntityContainer (String name) {
+	    return entityContainers.get(name);}
+	public void addAssociation (AtomicSchema parent, DatabaseMetaData m, ResultSet r) throws SQLException {
 	    if (!associations.containsKey(r.getString(2))) {
-		AtomicAssociation a = new AtomicAssociation(m, r);
-		associations.put(a.getName(), a);}}
-	public void addEnd2Association (DatabaseMetaData m, ResultSet r) throws SQLException {
-	    if (!associations.containsKey(r.getString(6))) {
-		AtomicAssociation a = new AtomicAssociation(m, r);
+		AtomicAssociation a = new AtomicAssociation(parent, m, r);
 		associations.put(a.getName(), a);}}
 	public void addComplexTypes () {}
         public void addEntityContainer (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    if (!entityContainers.containsKey(r.getString(2))) {
                 AtomicEntityContainer ec = new AtomicEntityContainer(m, r);
                 entityContainers.put(ec.getName(), ec);}
-            if (m.getUserName().equals(r.getString(5)) && "select".equalsIgnoreCase(r.getString(6))) entityContainers.get(r.getString(2)).addEntitySet(m, r);}
+            if (m.getUserName().equals(r.getString(5)) && "select".equalsIgnoreCase(r.getString(6)))
+		entityContainers.get(r.getString(2)).addEntitySet(m, r);}
         public void addEntityType (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    if (!entityTypes.containsKey(r.getString(2))) {
                 AtomicEntityType et = new AtomicEntityType(m, r);
                 entityTypes.put(et.getName(), et);}}}
 
     public class AtomicAssociation extends Association {
-	public AtomicAssociation (DatabaseMetaData m, ResultSet r) throws SQLException {
+	public AtomicSchema schema;
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	public AtomicAssociation (AtomicSchema schema, DatabaseMetaData m, ResultSet r) throws SQLException {
 	    super();
-	    setName(r.getString(12));
-	    setEnd1(new AtomicAssociationEnd());
-	    setEnd2(new AtomicAssociationEnd());
-	    setReferentialConstraint(new AtomicReferentialConstraint());
+	    this.schema = schema;
+	    setName(r.getString(FK_NAME));
+	    setEnd1(new AtomicAssociationEnd(this, m, r, true));
+	    setEnd2(new AtomicAssociationEnd(this, m, r, false));
+	    setReferentialConstraint(new AtomicReferentialConstraint(this, m, r, getEnd1(), getEnd2()));
 	}}
 
     public class AtomicReferentialConstraint extends ReferentialConstraint {
-	public AtomicReferentialConstraint () {
+	public AtomicAssociation association;
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	public AtomicReferentialConstraint (AtomicAssociation association, DatabaseMetaData m, ResultSet r, AssociationEnd end1, AssociationEnd end2) {
 	    super();
-	    setAnnotationAttributes(new ArrayList<AnnotationAttribute>());
-	    setAnnotationElements(new ArrayList<AnnotationElement>());
-	    setDocumentation(new Documentation());
-	    setDependent(new AtomicReferentialConstraintRole());
-	    setPrincipal(new AtomicReferentialConstraintRole());}}
+	    this.association = association;
+	    setDependent(new AtomicReferentialConstraintRole(this, m, r, end2));
+	    setPrincipal(new AtomicReferentialConstraintRole(this, m, r, end1));}}
 
     public class AtomicReferentialConstraintRole extends ReferentialConstraintRole {
-	public AtomicReferentialConstraintRole () {
+	public AtomicReferentialConstraint referentialConstraint;
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	@Override public List<PropertyRef> getPropertyRefs () {
+	    return new ArrayList<PropertyRef>();}
+	public AtomicReferentialConstraintRole (AtomicReferentialConstraint referentialConstraint, DatabaseMetaData m, ResultSet r, AssociationEnd end) {
 	    super();
-	    setAnnotationAttributes(new ArrayList<AnnotationAttribute>());
-	    setAnnotationElements(new ArrayList<AnnotationElement>());
-	    setRole("foo");
+	    this.referentialConstraint = referentialConstraint;
+	    setRole(end.getRole());
 	    setPropertyRefs(new ArrayList<PropertyRef>());}}
 
     public class AtomicAssociationEnd extends AssociationEnd {
-	public AtomicAssociationEnd () {
+	public AtomicAssociation association;
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	public AtomicAssociationEnd (AtomicAssociation association, DatabaseMetaData m, ResultSet r, boolean end1) throws SQLException {
 	    super();
-	    setAnnotationAttributes(new ArrayList<AnnotationAttribute>());
-	    setAnnotationElements(new ArrayList<AnnotationElement>());
-	    setDocumentation(new Documentation());
+	    this.association = association;
+	    setRole(association.schema.getEntityContainer(r.getString(2)).getEntitySet(end1 ? r.getString(3) : r.getString(7)).getName());
 	    setMultiplicity(EdmMultiplicity.MANY);
-	    setOnDelete(new AtomicOnDelete());
-	    setRole("foo");
-	    setType(new FullQualifiedName("foo", "bar"));}}
+	    setType(association.schema.getEntityContainer(r.getString(2)).getEntitySet(end1 ? r.getString(3) : r.getString(7)).getEntityType());
+	    setOnDelete(new AtomicOnDelete(m, r));}}
 
     public class AtomicOnDelete extends OnDelete {
-	public AtomicOnDelete () {
+        private Map<String, AtomicAnnotationAttribute> annotationAttributes = new HashMap<>();
+        private Map<String, AtomicAnnotationElement> annotationElements = new HashMap<>();
+	@Override public List<AnnotationAttribute> getAnnotationAttributes () {
+	    return new ArrayList<AnnotationAttribute>(annotationAttributes.values());}
+	@Override public List<AnnotationElement> getAnnotationElements () {
+	    return new ArrayList<AnnotationElement>(annotationElements.values());}
+	public AtomicOnDelete (DatabaseMetaData m, ResultSet r) throws SQLException {
 	    super();
-	    setAction(EdmAction.Cascade);
-	    setAnnotationAttributes(new ArrayList<AnnotationAttribute>());
-	    setAnnotationElements(new ArrayList<AnnotationElement>());
-	    setDocumentation(new Documentation());}}
+	    setAction("importedKeyCascade".equals(r.getString(DELETE_RULE)) ? EdmAction.Cascade : EdmAction.None);}}
 
-    public class AtomicComplexType extends ComplexType {
-	public AtomicComplexType () {super();}}
+    public class AtomicAnnotationAttribute extends AnnotationAttribute {}
+
+    public class AtomicAnnotationElement extends AnnotationElement {}
+
+    public class AtomicComplexType extends ComplexType {}
+
+    public class AtomicUsing extends Using {}
 
     public class AtomicEntityType extends EntityType {
         public AtomicEntityType (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
@@ -123,7 +187,10 @@ public class BottomUpAtomicEdmProvider extends AtomicEdmProvider {
         public AtomicEntityContainer (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    super();
             setName("" + r.getString(2));}
-	@Override public List<EntitySet> getEntitySets () {return new ArrayList<EntitySet>(entitySets.values());}
+	@Override public List<EntitySet> getEntitySets () {
+	    return new ArrayList<EntitySet>(entitySets.values());}
+	public EntitySet getEntitySet (String name) {
+	    return entitySets.get(name);}
         public void addEntitySet (DatabaseMetaData m, ResultSet r) throws NamingException, SQLException {
 	    if (!entitySets.containsKey(r.getString(3))) {
                 AtomicEntitySet es = new AtomicEntitySet(m, r);
